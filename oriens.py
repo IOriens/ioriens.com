0c15f98bf327b5b2a41b3cd552e15a82024c8fbc
oriens <812561857@qq.com>
2015-10-19 11:02:02 -0700

    Add 404 Page

--- oriens.py
+++ oriens.py
@@ -14,6 +14,9 @@ def user(name):
 def threejs_demo1():
 	return render_template('webDemo/threejs1.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__=="__main__":	
 	app.run()

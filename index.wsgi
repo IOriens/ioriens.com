da2fd0273ed708170394d71833cc8b2c226ea495
oriens <812561857@qq.com>
2015-10-19 08:06:47 -0700

    Initial Commit

---
+++ index.wsgi
@@ -0,0 +1,3 @@
+import sae
+from oriens import app
+application = sae.create_wsgi_app(app)
---
+++ oriens.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+from flask import request
+#from flask.ext.script import Manager
+app = Flask(__name__)
+#manager=Manager(app)
+
+@app.route("/")
+def index():
+	#return "<h1>Hello World!</h1>"
+	#user_agent= request.headers.get('User-Agent')
+	#return '<p>Your browser is %s<p>'%user_agent
+	return render_template('index.html')
+
+@app.route("/user/<name>")
+def user(name):
+	return render_template('user.html',name=name)
+
+if __name__=="__main__":
+	#manager.run()
+	app.run()
